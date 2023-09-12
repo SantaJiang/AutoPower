f75762016dec637d0106e74ad43dae7df2de9d1b
jiangshengda <lilin.santa@gmail.com>
2023-09-12 15:57:02 +0800

    新增翻译

--- AutoPower_zh_CN.ts
+++ AutoPower_zh_CN.ts
@@ -1,3 +1,130 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>AutoPower</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>Task</source>
+        <translation>任务</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="147"/>
+        <location filename="mainwindow.ui" line="642"/>
+        <source>Shutdown</source>
+        <translation>关机</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="165"/>
+        <source>Reboot</source>
+        <translation>重启</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="177"/>
+        <source>Hibernate</source>
+        <translation>休眠</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="189"/>
+        <source>Sleep</source>
+        <translation>睡眠</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="204"/>
+        <source>Close Monitor</source>
+        <translation>黑屏</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="293"/>
+        <source>Time</source>
+        <translation>时间</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="321"/>
+        <source>Set time</source>
+        <translation>指定时间</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="370"/>
+        <source>yyyy/MM/dd</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="394"/>
+        <source>HH:mm</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="427"/>
+        <source>From now on</source>
+        <translation>从现在开始</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="489"/>
+        <source>h</source>
+        <translation>时</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="547"/>
+        <source>min</source>
+        <translation>分</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="627"/>
+        <source>Your device will automatically</source>
+        <translation>你的设备将</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="654"/>
+        <source>at</source>
+        <translation>在</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="687"/>
+        <source>19:00</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="733"/>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="105"/>
+        <source>minimized to tray</source>
+        <translation>最小化到任务栏</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="mainwindow.cpp" line="99"/>
+        <source>Running..</source>
+        <translation>运行中..</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="135"/>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="trayicon.cpp" line="68"/>
+        <source>MainWindow</source>
+        <translation>主界面</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="72"/>
+        <location filename="trayicon.cpp" line="76"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+</context>
+</TS>
